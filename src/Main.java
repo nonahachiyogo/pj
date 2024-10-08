3390266c4c8f2fbfd4a5ed8cd3a67f0d0dd2ca28
OleksandrDanshyn <s30942@pjwstk.edu.pl>
2024-10-08 16:39:13 +0200

    Finished Subtractor

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
   Adder adder = new Adder();
   System.out.println(adder.add(1, 2));
   Subtractor subtractor = new Subtractor();
-  System.out.println(subtractor.subtract(6, 3));
+  System.out.println(subtractor.subtract(10, 3));
  }
 }
