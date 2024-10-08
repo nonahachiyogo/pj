47a5d8d4fa6d8870919cb06de32230cb379ed78e
nonahachiyogo <s28385@pjwstk.edu.pl>
2024-10-08 16:39:09 +0200

    Adder ver2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
  public static void main(String[] args) {
   Adder adder = new Adder();
-  System.out.println(adder.add(1, 2));
+  System.out.println(adder.add(5, 2));
   Subtractor subtractor = new Subtractor();
   System.out.println(subtractor.subtract(6, 3));
  }
